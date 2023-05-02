f12c435189b7013b08b98dde8fd100d8de011244
sergey.malahov <sergey.malahov@brandmaker.com>
2023-05-02 15:10:55 +0200

    Cleanup; Small improvements

---
+++ spice-must-flow-spring/src/main/java/io/immortal/spicemustflowspring/SpiceMustFlowSpringApplication.java
@@ -0,0 +1,13 @@
+package io.immortal.spicemustflowspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiceMustFlowSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiceMustFlowSpringApplication.class, args);
+    }
+
+}
---
+++ spice-must-flow-spring/src/test/java/io/immortal/spicemustflowspring/SpiceMustFlowSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.immortal.spicemustflowspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiceMustFlowSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
