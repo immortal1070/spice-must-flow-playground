6f0f0d30b6d77767466f431d3e97ee9908ed33f8
sergey.malahov <sergey.malahov@brandmaker.com>
2023-05-03 23:18:21 +0200

    Cleanup

--- spice-must-flow-ktl/src/main/kotlin/io/immortal/spicemustflow/domain/ingredient/IngredientFindParams.kt
+++ spice-must-flow-ktl/src/main/kotlin/io/immortal/spicemustflow/domain/ingredient/IngredientFindParams.kt
@@ -1,19 +1,14 @@
 package io.immortal.spicemustflow.domain.ingredient
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springdoc.core.annotations.ParameterObject
 import java.util.*
 
-//class IngredientFindParams(
-//    ids: List<UUID>? = null,
-//    names: List<String>? = null
-//) {
-//    // TODO rename param to "id"
-//    var ids: List<UUID>? = ids
-//    var names: List<String>? = names
-//}
 @ParameterObject
 class IngredientFindParams (
     // TODO rename param to "id"
+    @JsonProperty("id")
     val ids: List<UUID>? = null,
+    @JsonProperty("name")
     val names: List<String>? = null
 )
